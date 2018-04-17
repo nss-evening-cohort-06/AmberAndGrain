14b2dc591e9a1603893d1b844263b937c024d2db
nathan gonzalez <ncgonz@gmail.com>
2018-04-16 20:22:17 -0500

    angular stuff is working...git status!

--- AmberAndGrain/App_Start/BundleConfig.cs
+++ AmberAndGrain/App_Start/BundleConfig.cs
@@ -22,6 +22,12 @@ namespace AmberAndGrain
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                "~/Scripts/angular.js",
+                "~/Scripts/angular-route.js",
+                "~/app/app.js")
+                .IncludeDirectory("~/app/controllers", "*.js", true));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
---
+++ AmberAndGrain/app/app.js
@@ -0,0 +1,10 @@
+﻿var app = angular.module("AmberAndGrain", ["ngRoute"]);
+
+
+app.config(["$routeProvider",function ($routeProvider) {
+    $routeProvider.when("/",
+        {
+            templateUrl: "/app/partials/index.html",
+            controller: "HomeController"
+        });
+}]);
